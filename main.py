e437f7b7b16ad5d0e6775803596181841bff6dc7
Nawaz Gafar <gafar.nawaz@gmail.com>
2021-02-23 15:21:28 -0500

    added function to get photo ids

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import requests
 from secrets import API_KEY
 
 def get_group_id(group_url):
+    # https://www.flickr.com/services/api/flickr.urls.lookupGroup.html
+
     url = f'https://www.flickr.com/services/rest/?method=flickr.urls.lookupGroup&api_key={API_KEY}&url={group_url}&format=json&nojsoncallback=1'
 
     r = requests.get(url).json()
@@ -9,6 +11,24 @@ def get_group_id(group_url):
 
     return group_id
 
+def get_photo_ids(group_id):
+    # https://www.flickr.com/services/api/flickr.photos.search.html
+
+    url = f'https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key={API_KEY}&group_id={group_id}&format=json&nojsoncallback=1&page=1&per_page=500'
+
+    r = requests.get(url).json()
+
+    # Flickr paginates the resuts. Default is 100 results per page, max is 500.
+    total_pages = r['photos']['pages']
+
+    pic_ids = []
+    for pic in r['photos']['photo']:
+        pic_ids.append(pic['id'])
+
+    return pic_ids
+
 group_url = 'https://www.flickr.com/groups/velvia50/pool/'
 group_id = get_group_id(group_url)
-print(group_id)
+
+photo_ids = get_photo_ids(group_id)
+print(photo_ids)
